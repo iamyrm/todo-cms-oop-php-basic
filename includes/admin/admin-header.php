6aa5886294c172e8d84b1a95a5b004fce7817d99
iamyrm <majhiyagya123@gmail.com>
2026-03-03 16:08:11 +0545

    init files added for initialization

--- includes/admin/admin-header.php
+++ includes/admin/admin-header.php
@@ -1,3 +1,8 @@
+<?php
+include __DIR__ . "/../../init.php";
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
---
+++ init.php
@@ -0,0 +1,9 @@
+<?php
+// Start Session
+session_start();
+
+// Include Helper Functinos
+require_once 'functions/helpers.php';
+
+// Defile global constants
+define("APP_NAME", "CMS PDO SYSTEM");
