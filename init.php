db7056f74dace4602f137bc63800cc354ef33477
iamyrm <majhiyagya123@gmail.com>
2026-03-05 12:15:04 +0545

    Autoloader Created

--- init.php
+++ init.php
@@ -1,12 +1,13 @@
 <?php
+// Autoload Classes
+require_once 'functions/autoloader.php';
+
 // Start Session
 session_start();
 
 // Including  config
 require_once 'config/config.php';
 
-// Loadig database
-require_once 'classes/Database.php';
 
 // Include Helper Functinos
 require_once 'functions/helpers.php';
